4adce6d16f6380713bbe2b6ff444a7177c49edd6
Samantha Hyatt <shhyatt@gmail.com>
2019-07-15 21:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,9 +27,17 @@ function findOrAdd(currentNode, newNode){
     if(currentNode.right){
       return findOrAdd(currentNode.right, newNode)
     }else{
-      return currentNode.right = newNode 
+      
+      return currentNode.right = newNode; 
     }
     
+  }
+  if(currentNode.data > newNode.data){
+    if(currentNode.left){
+      return findOrAdd(currentNode.left, newNode)
+    }else{
+      return currentNode.left = newNode;
+    }
   }
   
 }
